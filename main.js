75b4fe54f0ef5d6c54574e100e645aeb11de92a5
m4isv <53246704+m4isv@users.noreply.github.com>
2023-03-13 16:04:38 -0300

    music

---
+++ main.js
@@ -0,0 +1,7 @@
+document.body.style.backgroundImage = "url('https://i.pinimg.com/originals/d0/28/3f/d0283f4eb78abd248935d692d0d96f29.gif')";
+
+document.body.style.backgroundRepeat = "no-repeat"
+
+document.body.style.backgroundSize = "cover"
+
+document.body.style.backgroundAttachment = "fixed"
